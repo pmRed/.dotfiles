c652b4fa3ea54d096344bba852b90a6e10526505
Prithvi Reddy <u5021997@anu.edu.au>
2017-12-23 16:48:36 +1100

    added emacs

---
+++ emacs/elpa/general-20171208.9/general-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "general" "20171208.9" "Convenience wrappers for keybindings." '((emacs "24.4") (cl-lib "0.5")) :commit "bb9eb875b4cc8bbed1489a10d473a7f33f5461ff" :url "https://github.com/noctuid/general.el" :keywords '("vim" "evil" "leader" "keybindings" "keys"))
